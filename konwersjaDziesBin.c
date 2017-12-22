652fde14337963477ca7f37986da417c08d0d958
wspolPP <piotrekpo.46@gmail.com>
2017-12-22 10:08:36 +0100

    Zmiany kosmetyczne w kodzie konwersji.

--- konwersjaDziesBin.c
+++ konwersjaDziesBin.c
@@ -3,21 +3,21 @@
 int main()
 {
 	int liczba;
-	printf("Podaj liczba do konwersji: ");
+	printf("Podaj liczbe dziesietna do konwersji: ");
 	scanf("%d" ,&liczba);
-	printf("Wynik: \n");
+	printf("\nWynik: \n");
 	if(liczba==0)
 	{
 		printf("0\n");
 		return 0;
 	}
-	while(liczba /2 !=0){
+	while(liczba  >0){
 		printf("%d",liczba %2);
 		liczba /=2;
 		}
 		
-		printf("    1");
-		printf("\nWynik podany w kolejnosci odwrotnej \n");
+		printf("\n");
+		printf("\nWynik binarny odczytuj w kolejnosci cyfr odwrotnej \n");
 		return 0;
 		
 }
